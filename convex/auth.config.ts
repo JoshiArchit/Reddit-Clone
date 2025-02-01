69f2d481d312749484070543c8e7925b30f11b8f
Archit Joshi <joshi.archit.2904@gmail.com>
2025-02-01 12:34:58 -0600

    Convex webhooks and auth setup

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "{https://tight-raven-42.clerk.accounts.dev}",
+      domain: "https://tight-raven-42.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
--- convex/schema.ts
+++ convex/schema.ts
@@ -1,13 +1,18 @@
 /*
 Json strict schema for convex.
+Set up webhooks for Clerk to notify Convex of user events - https://docs.convex.dev/auth/database-auth#set-up-webhooks
+Convex dashboard for setting env variables, etc. - https://dashboard.convex.dev/
 */
 
 import { defineSchema, defineTable } from "convex/server";
-import {v} from 'convex/values';
+import { v } from "convex/values";
 
 export default defineSchema({
-    users: defineTable({
-        username: v.number(),
-        externalId: v.string()
-    }),
+  users: defineTable({
+    // Attributes connect local schema to Clerk schema
+    username: v.string(),
+    externalId: v.string(),
+  })
+    .index("byExternalId", ["externalId"])
+    .index("byUsername", ["username"]), // Indexing by externalId
 });
