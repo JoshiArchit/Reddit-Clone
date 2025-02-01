c2a24a2f99b576c9e405b9cf9a6e63c568f08527
Archit Joshi <joshi.archit.2904@gmail.com>
2025-02-01 14:02:17 -0600

    Route setup and page more stubs

--- src/pages/SubmitPage.tsx
+++ src/pages/SubmitPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import "../styles/SubmitPage.css";
 
 const SubmitPage = () => {
   return (
--- src/pages/SubredditPage.tsx
+++ src/pages/SubredditPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import "../styles/SubredditPage.css";
 
 const SubredditPage = () => {
   return (
